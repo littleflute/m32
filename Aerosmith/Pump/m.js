dc47bc210c3249413294812eb022892a49f2cb62
littleflute <13699175164@sina.cn>
2017-07-08 16:24:27 -0500

    Add files via upload

---
+++ Aerosmith/Pump/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Young Lust.mp3";		
+f[2]	= "03 Love in an Elevator.mp3";		
+f[3]	= "04 Monkey on My Back.mp3";		
+f[4]	= "05 Janie's Got a Gun.mp3";		
+f[5]	= "06 The Other Side.mp3";		
+f[6]	= "07 My Girl.mp3";		
+f[7]	= "08 Don't Get Mad, Get Even.mp3";		
+f[8]	= "09 Hoodoo-Voodoo Medicine Man.mp3";		
+f[9]	= "10 What It Takes.mp3";		
+f[10]	= "F.I.N.E.mp3";		
